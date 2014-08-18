e1606bdd20dbe9571ce1c09c982901e8dd230f7b
florian <florianEBI@users.noreply.github.com>
2014-08-18 14:08:17 +0000

    move archive redirections package to new location

---
+++ src/main/java/uk/ac/ebi/pride/archive/redirections/controller/PrideLegacyRedirectionsController.java
@@ -0,0 +1,58 @@
+package uk.ac.ebi.pride.archive.redirections.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author Florian Reisinger
+ * @since 1.0
+ */
+@Controller
+public class PrideLegacyRedirectionsController {
+
+    private static Logger accessLog = LoggerFactory.getLogger("uk.ac.ebi.pride.archive.redirections.AccessLogger");
+
+    public final static String PRIDE_LEGACY_URL = "/legacy";
+
+
+    @RequestMapping(value = "/directLink.do", method = RequestMethod.GET)
+    public org.springframework.web.servlet.ModelAndView prideDirectExperimentLinkToPrideLegacy(@RequestParam("experimentAccessionNumber") String accession) {
+        String target = "/directLink.do?experimentAccessionNumber=" + accession;
+        accessLog.info("FROM " + target + " TO: " + PRIDE_LEGACY_URL + target);
+        return new ModelAndView("redirect:" + PRIDE_LEGACY_URL + target);
+    }
+
+    @RequestMapping(value = "/experimentLink.do", method = RequestMethod.GET)
+    public org.springframework.web.servlet.ModelAndView prideExperimentLinkToPrideLegacy(@RequestParam("experimentAccessionNumber") String accession) {
+        String target = "/experimentLink.do?experimentAccessionNumber=" + accession;
+        accessLog.info("FROM " + target + " TO: " + PRIDE_LEGACY_URL + target);
+        return new ModelAndView("redirect:" + PRIDE_LEGACY_URL + target);
+    }
+
+    @RequestMapping(value = "/experiment.do", method = RequestMethod.GET)
+    public org.springframework.web.servlet.ModelAndView prideExperimentToPrideLegacy(@RequestParam("experimentAccessionNumber") String accession) {
+        String target = "/experiment.do?experimentAccessionNumber=" + accession;
+        accessLog.info("FROM " + target + " TO: " + PRIDE_LEGACY_URL + target);
+        return new ModelAndView("redirect:" + PRIDE_LEGACY_URL + target);
+    }
+
+    @RequestMapping(value = "/prideMart.do", method = RequestMethod.GET)
+    public org.springframework.web.servlet.ModelAndView prideBioMartLinkToPrideLegacy() {
+        String target = "/prideMart.do";
+        accessLog.info("FROM " + target + " TO: " + PRIDE_LEGACY_URL + target);
+        return new ModelAndView("redirect:" + PRIDE_LEGACY_URL + target);
+    }
+
+    @RequestMapping( value = "/searchSummary.do", method = RequestMethod.GET)
+    public org.springframework.web.servlet.ModelAndView prideSearchToPrideLegacy(@RequestParam("queryTypeSelected") String type, @RequestParam("identificationAccessionNumber") String accession) {
+        String target = "/searchSummary.do?queryTypeSelected=" + type + "&identificationAccessionNumber=" + accession;
+        accessLog.info("FROM " + target + " TO: " + PRIDE_LEGACY_URL + target);
+        return new ModelAndView("redirect:" + PRIDE_LEGACY_URL + target);
+    }
+
+}
